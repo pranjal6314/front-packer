ef4ee171c9f6cef291894d5711647b21a65f57b6
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2023-05-26 18:03:57 +0530

    new version

---
+++ models/User.js
@@ -0,0 +1,19 @@
+// getting-started.js
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI);
+}
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+mongoose.models = {};
+export default mongoose.model("User", UserSchema);
+// export default mongoose.model.User || mongoose.model("User", UserSchema);
