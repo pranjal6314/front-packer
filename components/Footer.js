de518d1964a0cd63d804ae53a4f15fc8a4149e47
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2023-06-03 09:15:43 +0530

    new version

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+
+const Footer = () => {
+  return <div></div>;
+};
+
+export default Footer;
---
+++ pages/api/signup.js
@@ -0,0 +1,25 @@
+import User from "../../models/User";
+import connectDb from "../../middleware/monooges";
+var CryptoJS = require("crypto-js");
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const { name, email, password, phone, pin, gstin, address, pan } = req.body;
+    let u = new User({
+      name,
+      email,
+      password: CryptoJS.AES.encrypt(req.body.password, "sec1234").toString(),
+      phone,
+      pin,
+      gstin,
+      address,
+      pan,
+    });
+
+    await u.save();
+    res.status(200).json({ success: "success" });
+  } else {
+    res.status(400).json({ message: "We only support POST" });
+  }
+};
+
+export default connectDb(handler);
