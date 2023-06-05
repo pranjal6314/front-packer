fef37d566b01699f0b7cd188053eb72f46512877
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2023-06-05 20:01:33 +0530

    new  version

---
+++ pages/api/getuser.js
@@ -0,0 +1,21 @@
+import User from "../../models/User";
+import Bill from "../../models/Bill";
+import connectDb from "../../middleware/monooges";
+var CryptoJS = require("crypto-js");
+import jsonewebtoken from "jsonwebtoken";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    let token = req.body.token;
+    const user = jsonewebtoken.verify(token, "ourkey");
+    let dbuser = await User.findOne({ email: user.email });
+    const { name, address, phone, email, pin, gstin, pan } = dbuser;
+    res
+      .status(200)
+      .json({ name, address, phone, email, pin, gstin, phone, pan });
+  } else {
+    res.status(400).json({ success: false, error: "no user found" });
+  }
+};
+
+export default connectDb(handler);
